206ebce90d1ee7da8328574b414ed33cce5496d2
emmanuelmaldonado <andymaldonadolop60@gmail.com>
2026-03-05 22:43:28 -0600

    ejercicio veinte, el ultimo usando do while

--- Actividad 16/Program.cs
+++ Actividad 16/Program.cs
@@ -1,11 +1,28 @@
-﻿// pide un numero al usuario y mostrar su tabla de multiplicar del 1 al 10
+﻿// pide un numero al usuario hasta ingresar 0 y al final muestre:
 
-Console.WriteLine("Ingrese un númro");
-int numero = int.Parse(Console.ReadLine());
-int i = 1;
+int numero;
+int suma= 0;
+int cantidad = 0;
 do
 {
-    Console.WriteLine($"{numero} x {i} = {numero * i}");
-    i++;
+    Console.WriteLine("Ingrese un númro (0 para salir)");
+    numero = int.Parse(Console.ReadLine());
+    suma += numero;
+
+    if (numero != 0)
+    {
+        cantidad++;
+    } 
 }
-while (i <= 10);
+while (numero != 0);
+if (cantidad > 0)
+{
+    double promedio = suma / cantidad;
+    Console.WriteLine($"Suma total: {suma}");
+    Console.WriteLine($"Cantidad de números: {cantidad }");
+    Console.WriteLine($"Promedio:  {promedio}");
+}
+else
+{
+    Console.WriteLine("Error");
+}
