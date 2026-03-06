e21f0476d440bce1da0e9570da17c770807b6cc6
emmanuelmaldonado <andymaldonadolop60@gmail.com>
2026-03-05 21:02:16 -0600

    ejercicio quince usando do while

--- Actividad 16/Program.cs
+++ Actividad 16/Program.cs
@@ -1,15 +1,10 @@
 ﻿// pida números al usuario hasta ingresar 0 y determine cual fue el nuero mayor 
 
 int numero;
-int mayor = 0;
 do
 {
-    Console.WriteLine("Ingrese un número (0 para salir)");
+    Console.WriteLine("Ingrese un número positivo");
     numero = int.Parse(Console.ReadLine());
-    if (numero > mayor && numero != 0)
-    {
-    mayor = numero;
-    }
 }
-while (numero != 0);
-Console.WriteLine($"Número mayor {mayor}");
+while (numero <= 0);
+Console.WriteLine($"Número aceptado {numero}");
